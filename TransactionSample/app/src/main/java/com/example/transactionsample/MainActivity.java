fa0f2c572871acb51a2847b0f93bbf33b84cdef7
snavaneethan1 <navaneethan.saravanan@gmail.com>
2017-03-16 22:36:48 +0000

    Intent Samples

---
+++ TransactionSample/app/src/main/java/com/example/transactionsample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.transactionsample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://www.example.com"));
+                startActivity(i);
+            }
+        });
+
+        b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("tel:9510300000"));
+                startActivity(i);
+            }
+        });
+    }
+}
