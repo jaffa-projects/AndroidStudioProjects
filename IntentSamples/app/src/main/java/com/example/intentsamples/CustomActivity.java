df12598051ed8b712eab277b91274a07c09bc558
snavaneethan1 <snavaneethan@tapestry.local>
2017-03-17 15:41:36 +0000

    Intent Samples

---
+++ IntentSamples/app/src/main/java/com/example/intentsamples/CustomActivity.java
@@ -0,0 +1,22 @@
+package com.example.intentsamples;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class CustomActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_custom);
+        TextView label = (TextView) findViewById(R.id.show_data);
+        Uri url = getIntent().getData();
+        label.setText(url.toString());
+    }
+}
