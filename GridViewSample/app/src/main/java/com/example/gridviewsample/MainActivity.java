7c1811ed5ad9d6f665c42d71ed6b0423c6cd1100
snavaneethan1 <snavaneethan@tapestry.local>
2017-03-24 11:04:12 +0000

    GridView sample

---
+++ GridViewSample/app/src/main/java/com/example/gridviewsample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gridviewsample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gv = (GridView)findViewById(R.id.gridview);
+        gv.setAdapter(new ImageAdapter(this));
+
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id){
+                // Send intent to SingleViewActivity
+                Intent i = new Intent(getApplicationContext(), SingleViewActivity.class);
+                // Pass image index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
